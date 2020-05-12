0a16b555f9dae3fedc4ca2f1fe267553c37c6a25
Alegruz <alegruz@khu.ac.kr>
2020-05-12 18:34:02 +0900

    [feature] phong model implemented

--- app/src/main/assets/fragment.glsl
+++ app/src/main/assets/fragment.glsl
@@ -11,42 +11,48 @@ uniform vec3 srcDiffL, srcSpecL, srcAmbiL;
 uniform vec3 srcDiffR, srcSpecR, srcAmbiR;
 uniform float threshold;
 
-in vec3 v_normal;
+//in vec3 v_normal;
 in vec2 v_texCoord;
 in vec3 v_view, v_lightL, v_lightR;
 in float v_attL, v_attR;
+in vec3 v_diffL, v_diffR;
+in vec3 v_reflL, v_reflR;
 
 layout(location = 0) out vec4 fragColor;
 
-void main() {
-
-    vec3 color = texture(textureDiff, v_texCoord).rgb;
-
+void main()
+{
     // re-normalize unit vectors (normal, view, and light vectors)
-    // vec3 normal = ;
-    // vec3 view = ;
-    // vec3 lightL = ;
-    // vec3 lightR = ;
+//    vec3 normal = normalize( v_normal );
+    vec3 view = normalize( v_view );
+    vec3 lightL = normalize( v_lightL );
+    vec3 lightR = normalize( v_lightR );
+    vec3 reflL = normalize( v_reflL );
+    vec3 reflR = normalize( v_reflR );
 
     // diffuse term
-    // vec3 matDiff = ;
-    // vec3 diffL = ;
-    // vec3 diffR = ;
-    // vec3 diff = ;
+    vec4 albedo = texture( textureDiff, v_texCoord );
+//    vec3 diffL = max( v_diffL, 0.0 ) * srcDiffL * matDiff;
+//    vec3 diffR = max( v_diffR, 0.0 ) * srcDiffR * matDiff;
+    vec3 diffL = max( v_diffL, 0.0 ) * srcDiffL;
+    vec3 diffR = max( v_diffR, 0.0 ) * srcDiffR;
+    vec3 diff = clamp( (diffL + diffR) * albedo.xyz, 0.0, 1.0 );
 
     // specular term
-    // vec3 reflL = ;
-    // vec3 reflR = ;
-    // vec3 specL = ;
-    // vec3 specR = ;
-    // vec3 spec = ;
+//    vec3 reflL = 2.0 * normal * dot( normal, lightL ) - lightL;
+//    vec3 reflR = 2.0 * normal * dot( normal, lightR ) - lightR;
+
+    vec3 specL = pow( clamp( max( dot( reflL, -view ), 0.0), 0.0, 1.0 ), matSh ) * srcSpecL;
+    vec3 specR = pow( clamp( max( dot( reflR, -view ), 0.0), 0.0, 1.0 ), matSh ) * srcSpecR;
+//    float specularValueL = clamp( dot( reflL, -view ), 0.0, 1.0 );
+//    float specularValueR = clamp( dot( reflR, -view ), 0.0, 1.0 );
+//    vec3 spec = vec3( specularValueL + specularValueR );
+    vec3 spec = ( specL + specR ) * matSpec;
 
     // ambient term
-    // vec3 ambiL = ;
-    // vec3 ambiR = ;
-    // vec3 ambi = ;
-
-    // color = ;
+    vec3 ambiL = srcAmbiL;
+    vec3 ambiR = srcAmbiR;
+    vec3 ambi = ( ambiL + ambiR ) * matAmbi;
 
     float alpha = 1.0f;
 
@@ -56,5 +62,5 @@ void main() {
     //    alpha = 0.0;
 
     // final output color with alpha
-    fragColor = vec4(color, alpha);
+    fragColor = vec4(diff + spec + ambi + matEmit, alpha);
 }
--- app/src/main/assets/vertex.glsl
+++ app/src/main/assets/vertex.glsl
@@ -10,28 +10,36 @@ layout(location = 1) in vec3 normal;
 layout(location = 2) in vec2 texCoord;
 layout(location = 3) in vec3 tangent;
 
-out vec3 v_normal;
+//out vec3 v_normal;
 out vec2 v_texCoord;
 out vec3 v_view, v_lightL, v_lightR;
 out float v_attL, v_attR;
+out vec3 v_diffL, v_diffR;
+out vec3 v_reflL, v_reflR;
 
-void main() {
-
+void main()
+{
     // vertex displacement
     vec3 displacedPos = position;
 
     // world-space position
-    vec3 worldPos = (worldMat * vec4(displacedPos, 1.0)).xyz;
+    vec3 worldPos = ( worldMat * vec4( displacedPos, 1.0 ) ).xyz;
 
     // world-space vertex normal
-    v_normal = normalize(transpose(inverse(mat3(worldMat))) * normal);
+//    vec3 worldNormal = normalize(transpose(inverse(mat3(worldMat))) * normal);
+    vec3 worldNormal = normalize( mat3(worldMat) * normal );
 
     // view vector
-    v_view = normalize(eyePos - worldPos);
+    v_view = normalize( eyePos - worldPos );
 
     // light vectors
-    // v_lightL = ;
-    // v_lightR = ;
+    v_lightL = normalize( worldPos - lightPosL );
+    v_lightR = normalize( worldPos - lightPosR );
+
+    v_diffL = vec3( dot( -v_lightL, worldNormal ) );
+    v_diffR = vec3( dot( -v_lightR, worldNormal ) );
+    v_reflL = 2.0 * worldNormal * dot( worldNormal, v_lightL ) - v_lightL;
+    v_reflR = 2.0 * worldNormal * dot( worldNormal, v_lightR ) - v_lightR;
 
     // attenuations
     // float distL = ;
